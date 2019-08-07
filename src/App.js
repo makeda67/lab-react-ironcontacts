8a32624210984803cfc25e34878a3c14a3762900
Aleix Maqueda <aleixmaqueda97@gmail.com>
2019-08-07 11:12:19 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import contacts from './data/contacts.json';
 import './App.css';
 
 class App extends Component {
+  state = {
+    myContacts: contacts.splice(0,5),
+    contacts,
+  }
+  
+  handleNewRandomContact = () => {
+    const {contacts, myContacts} = this.state;
+   
+    const randomIndex = Math.round(Math.random() * contacts.length -1);
+    const newContact = contacts[randomIndex];
+    
+    const newMyContacts = [...myContacts];
+    newMyContacts.push(newContact);
+    this.setState({
+      myContacts: newMyContacts,
+    })
+  }
+
+  handleSortByPopularity = () => {
+    const {myContacts} = this.state;
+    const newMyContacts = [...myContacts];
+    newMyContacts.sort((a,b) => {
+      return b.popularity - a.popularity
+    });
+    this.setState({
+      myContacts: newMyContacts
+    })
+  }
+
+  handleSortByName = () => {
+    const {myContacts} = this.state;
+    const newMyContacts = [...myContacts];
+    
+    newMyContacts.sort((a,b) => {
+        if(a.name < b.name) { 
+        return -1; 
+      }
+      if(a.name > b.name) { 
+        return 1; 
+      }
+      return 0;
+    })
+    
+    this.setState({
+      myContacts: newMyContacts
+    })
+  }
+
+
+  handleDeleteContact = (index) => {
+    const {myContacts} = this.state;
+    const newMyContacts = [...myContacts];
+    
+    newMyContacts.splice(index,1);
+    this.setState({
+      myContacts: newMyContacts
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>IronContacts</h1>
+        <button onClick={this.handleNewRandomContact}>Add Random Contact</button>
+        <button onClick={this.handleSortByPopularity}>Sort by popularity</button>
+        <button onClick={this.handleSortByName}>Sort by name</button>
+          <table>
+            <thead>
+              <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+              </tr>
+            </thead>
+            <tbody>
+              
+                {
+                  this.state.myContacts.map((contact, index) => {
+                    return (
+                      <tr key={index}>
+                        <td><img src={contact.pictureUrl} alt={contact.name} /></td>
+                        <td><p>{contact.name}</p></td>
+                        <td><p>{contact.popularity}</p></td>
+                        <td><button onClick={() => {this.handleDeleteContact(index)}}>Delete</button></td>
+                      </tr>
+                    )
+                  })
+                }
+            </tbody>
+          </table>
       </div>
     );
   }
